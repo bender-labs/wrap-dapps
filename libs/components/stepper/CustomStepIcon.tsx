9cdb3511697de304db9419a8f53d172c27408e18
Sinazo Bogicevic <sinazo@Sinazos-MacBook-Pro.local>
2021-07-16 12:42:25 +0200

    feat:edit CustomStepIcon

--- libs/components/stepper/CustomStepIcon.tsx
+++ libs/components/stepper/CustomStepIcon.tsx
@@ -1,30 +1,7 @@
-import { createStyles, StepIconProps } from '@material-ui/core';
-import React from 'react';
+import { StepIconProps, Box } from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
-import clsx from 'clsx';
-import theme from '../theme/theme';
+import theme from "../theme/theme";
 
-
-  const root =  {
-    backgroundColor: '#FFFFFF',
-    zIndex: 1,
-    color: '#000',
-    fontWeight: 900,
-    width: 32,
-    height: 32,
-    display: 'flex',
-    borderRadius: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-
-  const active = {
-    backgroundColor: '#FFFFFF',
-  };
-
-  completed: {
-    backgroundColor: theme.palette.primary.main
-  };
 
 export default function CustomStepIcon(props: StepIconProps) {
   const {active, completed} = props;
@@ -35,13 +12,23 @@ export default function CustomStepIcon(props: StepIconProps) {
   };
 
   return (
-    <div
-      className={clsx(root, {
-        'active': active,
-        'completed': completed,
-      })}
+    <Box
+        sx={{
+          backgroundColor: '#FFFFFF',
+          zIndex: 1,
+          color: '#000',
+          fontWeight: 900,
+          width: 32,
+          height: 32,
+          display: 'flex',
+          borderRadius: '50%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          '.MuiStepIcon-active': {...active&&{backgroundColor: '#FFFFFF'}},
+          '.MuiStepIcon-completed': {...completed&&{backgroundColor: theme.palette.primary.main}}
+        }}
     >
       {icons[String(props.icon)]}
-    </div>
+    </Box>
   );
 }
