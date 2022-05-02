3f0d438d00f62278ad388f09f1ea5453c4b30f86
Charles Couillard <charles@benderlabs.io>
2022-05-02 11:25:13 +0200

    fix: change default Tezos RPC Node

--- libs/components/configuration/rpcNodes.ts
+++ libs/components/configuration/rpcNodes.ts
@@ -2,8 +2,8 @@ import { Environment, RPCNode } from './types';
 
 export const rpcNodes: { [key in Environment]: RPCNode[] } = {
   MAINNET: [
-    {name:'Tezos Foundation', url:'https://rpc.tzbeta.net/'},
     {name:'ECAD Labs', url:'https://mainnet.api.tez.ie'},
+    {name:'Tezos Foundation', url:'https://rpc.tzbeta.net/'},
     {name:'SmartPy', url:'https://mainnet.smartpy.io'},
     {name:'Giga Node', url:'https://mainnet-tezos.giganode.io'}
   ],
